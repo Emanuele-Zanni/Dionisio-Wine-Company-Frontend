ed5d89ab8b6834a9b283944cd8b1f62704abf173
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-07-31 01:00:57 +0200

    detalle+carrito

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -34,7 +34,7 @@ const Home = () => {
         <Carrusel />
       </div>
       <div className="text-center pt-28">
-      <h5 className="text-xl font-semibold">Nuestros Vinos Más Vendidos</h5>
+      <h5 className="text-2xl font-bold mb-4 text-center text-red-900">Nuestros Vinos Más Vendidos</h5>
         <div className="text-center pt-7">
           <ProductList products={products} />
         </div>
