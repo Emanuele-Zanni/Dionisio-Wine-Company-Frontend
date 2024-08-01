1cc523f4d5de88ef446a67727916e2daacdbac9b
Pedro Gandola <pedrogandola@hotmail.com>
2024-08-01 15:43:37 -0300

    trabajando

--- frontend/src/app/api/auth/[auth0]/route.js
+++ frontend/src/app/api/auth/[auth0]/route.js
@@ -1,42 +1,7 @@
-import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
-import axios from 'axios';
 
-const afterCallback = async (req, res, session) => {
-  try {
-    if (session && session.user) {
-      const { user } = session;
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-      // Enviar los datos del usuario a tu backend
-      await axios.post('api-vinos/users', {
-        id: user.sub,
-        name: user.name,
-        email: user.email
-      });
-    }
-  } catch (error) {
-    console.error('Error during afterCallback:', error);
-  }
-  return session;
-};
-
-export const GET = handleAuth({
-  callback: handleCallback(afterCallback)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export const GET = handleAuth();
 
 
 
