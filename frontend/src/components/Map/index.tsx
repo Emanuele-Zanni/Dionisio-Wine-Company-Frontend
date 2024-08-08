458b6ce2428bc9c36dadd69600af315ac0414cb3
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-08-08 23:49:53 +0200

    map

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
+
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import styles from './Map.module.css'
+import styles from './Map.module.css';
 
 // Configuración del mapa
 const mapContainerStyle = {
   height: "400px",
-  width: "400%"
+  width: "100%", // Corregí el ancho para que sea más adecuado para la visualización
 };
 
 const center = {
@@ -13,25 +14,21 @@ const center = {
   lng: -58.3736 // Longitud de la ubicación
 };
 
-
-const apiKey = 'AIzaSyDzh7T8ZPq52K6fZnYl_kfymEaBZ3AwFPs'
+const apiKey = 'AIzaSyDzh7T8ZPq52K6fZnYl_kfymEaBZ3AwFPs';
 
 const Map = () => {
   return (
-
-        <LoadScript googleMapsApiKey={apiKey}>
-          <div className={styles.mapContainer}>
-            <GoogleMap
-              mapContainerStyle={mapContainerStyle}
-              center={center}
-              zoom={13}
-              className={styles.map}  >
-              <Marker position={center} />
-            </GoogleMap>
-          </div>
-        </LoadScript>
-
-  
+    <LoadScript googleMapsApiKey={apiKey}>
+      <div className={styles.mapContainer}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={13}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </div>
+    </LoadScript>
   );
 };
 
