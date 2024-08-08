5cc416ab6cf7fe8e616ccc3afbe388d5d9a3538e
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-08-08 23:57:47 +0200

    plist

--- frontend/src/components/ProductList/index.tsx
+++ frontend/src/components/ProductList/index.tsx
@@ -6,7 +6,7 @@ function ProductList({ products }: IProductListProps) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16 p-8 mr-5">
       {products.map((product: IProduct) => (
-        <ProductsCard product={product} key={product.id} />
+        <ProductsCard product={product} key={product.productId} />
       ))}
     </div>
   );
