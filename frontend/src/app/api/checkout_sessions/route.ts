6ef0f0314db3c34ae7ad08b89a8baa043929dc69
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-08-12 12:26:50 +0200

    checkses

--- frontend/src/app/api/checkout_sessions/route.ts
+++ frontend/src/app/api/checkout_sessions/route.ts
@@ -4,14 +4,14 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-08-01',
+  apiVersion: '2024-06-20', // Actualiza la versión a la más reciente esperada o configurada
 });
 
 export async function POST(req: NextRequest) {
   try {
     const { cartItems } = await req.json();
 
-    console.log('Received cartItems:', cartItems); // Agregar esta línea para ver los datos recibidos
+    console.log('Received cartItems:', cartItems);
 
     if (!cartItems || cartItems.length === 0) {
       return NextResponse.json({ error: 'Cart is empty' }, { status: 400 });
@@ -29,7 +29,7 @@ export async function POST(req: NextRequest) {
       quantity: item.quantity,
     }));
 
-    console.log('Line items:', line_items); // Agregar esta línea para ver los elementos de línea
+    console.log('Line items:', line_items);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
