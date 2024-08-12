7c1839280bb755cff81dc44bf62b5a88f9c85121
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-08-12 14:33:00 +0200

    detail

--- frontend/src/app/detail/[id]/page.tsx
+++ frontend/src/app/detail/[id]/page.tsx
@@ -44,7 +44,7 @@ function Detail({ params }: { params: { productId: string } }) {
         return () => {
             isMounted = false;
         };
-    }, [params, params.productId]);
+    }, [params.productId]);
 
     if (loading) return <p>La página se está cargando...</p>;
     if (error) return <p>{error}</p>;
