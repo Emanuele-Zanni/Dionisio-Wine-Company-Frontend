661927d999d1f1cfd84734cb1327a053cf86abfe
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-08-15 13:16:32 +0200

    about-us

--- frontend/src/app/about-us/page.tsx
+++ frontend/src/app/about-us/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import Map from '@/components/Map';
+
+import Image from 'next/image';
 
 const About = () => {
   return (
@@ -12,27 +13,63 @@ const About = () => {
         <p className="text-lg text-gray-700 mb-6">
           Desde nuestros humildes comienzos, hemos crecido para convertirnos en un referente en la comercialización de vinos de alta calidad, ofreciendo una cuidadosa selección de etiquetas que reflejan la riqueza y diversidad de las regiones vitivinícolas argentinas. Cada botella que ofrecemos cuenta una historia, ya sea de un pequeño viñedo familiar en Mendoza, una bodega innovadora en Salta, o una añada excepcional de la Patagonia.
         </p>
-        <p className="text-lg text-gray-700 mb-6">
-          Nos enorgullece trabajar directamente con los productores, estableciendo relaciones de confianza y colaboración que nos permiten garantizar la autenticidad y excelencia de cada vino. Nuestro compromiso no es solo con la calidad, sino también con la sostenibilidad y la preservación de las tradiciones vinícolas.
-        </p>
-        <p className="text-lg text-gray-700 mb-6">
-          A lo largo de los años, hemos creado un espacio donde los amantes del vino pueden explorar, aprender y disfrutar. Nuestro equipo, apasionado y conocedor, está siempre dispuesto a guiar a nuestros clientes en su viaje por el fascinante mundo del vino, ya sea para descubrir nuevos sabores o para encontrar esa botella especial para una ocasión memorable.
-        </p>
-        <p className="text-lg text-gray-700 mb-6">
-          Hoy, seguimos comprometidos con nuestra misión de celebrar la cultura del vino, difundiendo la esencia y el carácter único de cada etiqueta que seleccionamos. Con una combinación de tradición y modernidad, buscamos ser la puerta de entrada a experiencias vinícolas excepcionales, tanto para conocedores como para aquellos que recién comienzan a descubrir la magia del vino.
-        </p>
+       
         <p className="text-lg text-gray-700 mb-6">
           Desde Buenos Aires, brindamos por el pasado, celebramos el presente y miramos con entusiasmo hacia el futuro, siempre con una copa en mano y el deseo de seguir compartiendo nuestra pasión por el vino con el mundo.
         </p>
-        <p className="text-xl text-end font-bold text-red-950">
-          Puedes visitarnos aquí!
-        </p>
-      </div>
-      <div className="w-full mt-6">
-        <Map />
-      </div>
+        <p className="text-xl text-center font-bold text-red-950"> Nuestro Equipo de Trabajo</p>
+         </div>
+    
+       <div className="mt-8 flex justify-center space-x-4">
+                    <a href="https://www.linkedin.com/in/mar%C3%ADa-laura-arcucci/" target="_blank" rel="noopener noreferrer">
+                        <Image 
+                            src="/lau.jpeg" 
+                            alt="Foto 1" 
+                            width={80} 
+                            height={80} 
+                            className="rounded-full object-cover"
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/in/pedro-gandola/" target="_blank" rel="noopener noreferrer">
+                        <Image 
+                            src="https://media.licdn.com/dms/image/v2/D4D35AQHabwkRFhoRXw/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1689255935737?e=1724266800&v=beta&t=PPQYH5-XXbdfDOiyXYt8-ImzjB7nK7SahmR66nHOp4U" 
+                            alt="Foto 2" 
+                            width={80} 
+                            height={80} 
+                            className="rounded-full object-cover"
+                        />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        <Image 
+                            src="" 
+                            alt="Foto 3" 
+                            width={80} 
+                            height={80} 
+                            className="rounded-full object-cover"
+                        />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        <Image 
+                            src="" 
+                            alt="Foto 4" 
+                            width={80} 
+                            height={80} 
+                            className="rounded-full object-cover"
+                        />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        <Image 
+                            src="" 
+                            alt="Foto 5" 
+                            width={80} 
+                            height={80} 
+                            className="rounded-full object-cover"
+                        />
+                    </a>
+                </div>
     </div>
   );
 };
+
 
 export default About;
