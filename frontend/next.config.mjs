6e019d5596006fbf7cadbde39e4cd19cd0e31348
Emanuele-Zanni <emazanni2022@gmail.com>
2024-08-15 05:44:43 -0300

    Fixed Login in Netlify Deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /**
  * @type {import('next').NextConfig}
  */
-export const nextConfig = {
+const nextConfig = {
   images: {
     domains: ["dummyimage.com"], // Permite imágenes desde dummyimage.com
     remotePatterns: [
@@ -14,11 +14,6 @@ export const nextConfig = {
   env: {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_SECRET: process.env.AUTH0_SECRET,
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
   },
   async rewrites() {
     return [
@@ -32,6 +27,4 @@ export const nextConfig = {
   },
 };
 
-export const experimental = {
-  appDir: true,
-};
+export default nextConfig;
