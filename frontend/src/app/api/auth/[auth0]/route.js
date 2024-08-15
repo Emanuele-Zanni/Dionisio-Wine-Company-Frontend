8bd73185667df8df6660c6b6b91180135ff2a005
Emanuele-Zanni <emazanni2022@gmail.com>
2024-08-15 03:47:32 -0300

    ya no se

--- frontend/src/app/api/auth/[auth0]/route.js
+++ frontend/src/app/api/auth/[auth0]/route.js
@@ -1,4 +1,6 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import * as info from "../../../../.env";
+import * as info2 from "../../../../.env.local";
 
 export const GET = handleAuth({
   login: handleLogin({
