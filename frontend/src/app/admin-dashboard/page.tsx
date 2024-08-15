0fbe998e84eff7b824daeb67b9632117cee2f9f2
Pedro Gandola <pedrogandola@hotmail.com>
2024-08-15 14:33:49 -0300

    pr manage

--- frontend/src/app/admin-dashboard/page.tsx
+++ frontend/src/app/admin-dashboard/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import UserManagement from "@/components/UserManagement";
 import DiscountCodeGenerator from "@/components/Discount";
-import { ProductManagement } from "@/components/ProductManagement";
+import  ProductManagement from "@/components/ProductManagement";
 
 const AdminDashboard: React.FC = () => {
   const { user } = useUser();
