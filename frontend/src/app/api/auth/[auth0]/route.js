9256ce3b279394110cb189f41a1e222094c366cd
Emanuele-Zanni <emazanni2022@gmail.com>
2024-08-15 03:53:23 -0300

    ya no se x2

--- frontend/src/app/api/auth/[auth0]/route.js
+++ frontend/src/app/api/auth/[auth0]/route.js
@@ -1,16 +1,10 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
-import * as info from "../../../../.env";
-import * as info2 from "../../../../.env.local";
 
 export const GET = handleAuth({
-  login: handleLogin({
-    returnTo: "/",
-  }),
-
-  signup: handleLogin({
-    authorizationParams: {
-      screen_hint: "signup",
-    },
-    returnTo: "/",
-  }),
+  // Configura el issuerBaseURL aquí si es necesario
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
